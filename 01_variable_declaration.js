04b1ca766b9cecab4a07281a1a66102ef003455e
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-03-18 17:34:23 +0400

    Initial commit

---
+++ 01_variable_declaration.js
@@ -0,0 +1,25 @@
+// ES5
+
+var myLocation = 'Default';
+function getLocation(ip) {
+    if (ip) {
+        var myLocation = getLocationByIp(ip);
+        return myLocation;
+    }
+    return myLocation;
+}
+console.log(getLocation());
+
+
+// ES6
+
+let myLocation = 'Default';
+function getLocation(ip) {
+    if (ip) {
+        let myLocation = getLocationByIp(ip);
+        return myLocation;
+    }
+    return myLocation;
+}
+console.log(getLocation());
+
