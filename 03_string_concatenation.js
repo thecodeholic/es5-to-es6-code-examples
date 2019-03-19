6b4dec52b66474d05f10ecc9d0baa55cc9282775
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-03-19 20:36:23 +0400

    String concatenation

---
+++ 03_string_concatenation.js
@@ -0,0 +1,42 @@
+// ES5
+
+var name = "Zura";
+var str = "Hello from " + name;
+
+var HTML5_SKELETON =
+'<!doctype html>\n' +
+'<html>\n' +
+'<head>\n' +
+'    <meta charset="UTF-8">\n' +
+'    <title></title>\n' +
+'</head>\n' +
+'<body>\n' +
+'</body>\n' +
+'</html>\n';
+
+var HTML5_SKELETON = '\
+    <!doctype html>\n\
+    <html>\n\
+    <head>\n\
+        <meta charset="UTF-8">\n\
+        <title></title>\n\
+    </head>\n\
+    <body>\n\
+    </body>\n\
+    </html>';
+
+// ES6
+
+const name = 'John';
+const str = `Hello from ${name}`;
+
+const HTML5_SKELETON = `
+    <!doctype html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    </body>
+    </html>`;
