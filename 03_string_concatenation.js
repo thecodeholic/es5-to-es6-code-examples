452653d489c803b3ddd14c8f99ddff1b203dc734
TheCodeholic <zurasekhniashvili@gmail.com>
2019-04-01 23:49:04 +0400

    Change string concatenation file

--- 03_string_concatenation.js
+++ 03_string_concatenation.js
@@ -1,42 +1,62 @@
 // ES5
 
-var name = "Zura";
-var str = "Hello from " + name;
+var domain = "localhost";
+var port = 3000;
+var id = 5;
+var url = 'http://' + domain + ':' + port + '/api/users/' + id;
 
-var HTML5_SKELETON =
-'<!doctype html>\n' +
-'<html>\n' +
-'<head>\n' +
-'    <meta charset="UTF-8">\n' +
-'    <title></title>\n' +
-'</head>\n' +
-'<body>\n' +
-'</body>\n' +
-'</html>\n';
+// Multiline strings
+// Version 1
+var paragraph = 'First paragraph\n'
+    + 'Second paragraph.';
 
-var HTML5_SKELETON = '\
-    <!doctype html>\n\
-    <html>\n\
-    <head>\n\
-        <meta charset="UTF-8">\n\
-        <title></title>\n\
-    </head>\n\
-    <body>\n\
-    </body>\n\
-    </html>';
+// Version 2
+var paragraph = 'First paragraph\n\
+        Second paragraph.';
+
+// var HTML5_SKELETON =
+//     '<!doctype html>\n' +
+//     '<html>\n' +
+//     '<head>\n' +
+//     '    <meta charset="UTF-8">\n' +
+//     '    <title></title>\n' +
+//     '</head>\n' +
+//     '<body>\n' +
+//     '</body>\n' +
+//     '</html>\n';
+
+// var HTML5_SKELETON = '\
+//     <!doctype html>\n\
+//     <html>\n\
+//     <head>\n\
+//         <meta charset="UTF-8">\n\
+//         <title></title>\n\
+//     </head>\n\
+//     <body>\n\
+//     </body>\n\
+//     </html>';
 
 // ES6
 
-const name = 'John';
-const str = `Hello from ${name}`;
+// Template literals
+var domain = "localhost";
+var port = 3000;
+var id = 5;
+var url = `http://${domain}:${port}/api/users/${id}`;
 
-const HTML5_SKELETON = `
-    <!doctype html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-    </body>
-    </html>`;
+// Multiline strings
+// Version 1
+var paragraph = `First paragraph
+    Second paragraph.`;
+
+
+// const HTML5_SKELETON = `
+//     <!doctype html>
+//     <html>
+//     <head>
+//         <meta charset="UTF-8">
+//         <title></title>
+//     </head>
+//     <body>
+//     </body>
+//     </html>`;
