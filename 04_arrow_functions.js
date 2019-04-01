7960db694f43bb87699c2f709f67c7643fd61952
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-04-01 20:49:20 +0400

    Add second example on arrow functions

--- 04_arrow_functions.js
+++ 04_arrow_functions.js
@@ -1,4 +1,44 @@
 // ES5
 
+// Example 1
+var application = {
+  debug: true,
 
+  init: function () {
+    var me = this;
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', function () {
+      console.log(this.debug); // This won't work
+      console.log(me.debug); // This WILL work
+    });
+  }
+}
+
+// Example 2
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var squareNumbers = numbers.map(function (n) {
+  return n * n;
+})
+
+// -----------------------------------
 // ES6
+// -----------------------------------
+
+// Example 1
+var application = {
+  debug: true,
+
+  init() {
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', ev => {
+      console.log(this.debug); // This WILL work
+    });
+
+    // Or this
+    button.addEventListener('click', ev => console.log(this.debug));
+  }
+}
+
+// Example 2
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var squareNumbers = numbers.map(n => n * n);
